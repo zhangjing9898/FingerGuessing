b041decedda5249db44c4b75a0a65e24a2b81a7e
zhangjing <1105249307@qq.com>
2019-04-13 10:50:37 +0800

    feat: add some img to resource

---
+++ src/StartScene.ts
@@ -0,0 +1,13 @@
+class StartScene extends egret.Sprite {
+
+    public constructor() {
+        super();
+        this.init();
+    }
+
+    private start_btn: egret.Bitmap;
+    
+    private init() {
+
+    }
+}
