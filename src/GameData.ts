48e06ccf0841cc7bbf917395e42feb9b4af84545
zhangjing <1105249307@qq.com>
2019-04-13 15:16:01 +0800

    feat: add gameData file

---
+++ src/GameData.ts
@@ -0,0 +1,11 @@
+class GameData {
+
+    // 获取舞台的高度和宽度
+    public static getStageWidth() {
+        return egret.MainContext.instance.stage.stageWidth;
+    }
+
+    public static getStageHeight() {
+        return egret.MainContext.instance.stage.stageHeight;
+    }
+}
--- src/StartScene.ts
+++ src/StartScene.ts
@@ -8,6 +8,7 @@ class StartScene extends egret.Sprite {
     private start_btn: egret.Bitmap;
     
     private init() {
-
+        let shape = new egret.Shape();
+        shape.graphics.beginFill(0xe63872);
     }
 }
